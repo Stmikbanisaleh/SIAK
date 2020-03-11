43d7b09cf31c90c9355ce835e3acaa55d47c34da
imamsatrianta <imamsatrianta@gmail.com>
2020-03-11 10:53:59 +0700

    view tampil

--- application/views/page/guru/index.php
+++ application/views/page/guru/index.php
@@ -622,7 +622,7 @@
 	//function show all Data
 	function show_data() {
 		$.ajax({
-			type: 'ajax',
+			type: 'POST',
 			url: '<?php echo site_url('guru/tampil') ?>',
 			async: true,
 			dataType: 'json',
--- application/views/page/jabatan/view.php
+++ application/views/page/jabatan/view.php
@@ -353,7 +353,7 @@
 	//function show all Data
 	function show_data() {
 		$.ajax({
-			type: 'ajax',
+			type: 'POST',
 			url: '<?php echo site_url('jabatan/tampil') ?>',
 			async: true,
 			dataType: 'json',
