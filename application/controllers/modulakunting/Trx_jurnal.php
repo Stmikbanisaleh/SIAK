5a5b33970d92fc106fa4b3584d4f1ecc588b8493
imamsatrianta <imamsatrianta@gmail.com>
2020-03-23 13:40:08 +0700

    fix jurnal

--- application/controllers/modulakunting/Trx_jurnal.php
+++ application/controllers/modulakunting/Trx_jurnal.php
@@ -61,7 +61,9 @@ class Trx_jurnal extends CI_Controller
 
     public function simpanjurnal()
     {
+        // print_r($this->input->post());exit;
         $kdo = $this->input->post('kdo');
+        $kod = $this->input->post('kod');
         $nopem = $this->input->post('nopem');
         if ($kdo == 1) {
             $data = $this->db->query("SELECT * FROM akuntansi WHERE bukti='$nopem'")->result();
@@ -76,41 +78,9 @@ class Trx_jurnal extends CI_Controller
                     'userid'    => $this->session->userdata('nip'),
                     'tgl_input' => date('Y-m-d')
                 );
-                $insert = $this->model_trx_jurnal->insert($mydata,'akuntansi');
-                if($insert){
-                    $datas = $this->db->query("SELECT
-                    jurnal.kode_jurnal,
-                    jurnal.nama_jurnal,
-                    (SELECT z.NAMA_REV FROM msrev z WHERE z.KETERANGAN=jurnal.JR AND z.`STATUS`=7) AS JR,
-                    (SELECT z.NAMA_REV FROM msrev z WHERE z.KETERANGAN=jurnal.type AND z.`STATUS`=8) AS type,
-                    jenispembayaran.Kodejnsbayar,
-                    jenispembayaran.namajenisbayar,
-                    detail_bayar_sekolah.nominalbayar,
-                    pembayaran_sekolah.tglentri,
-                    pembayaran_sekolah.NIS,
-                    pembayaran_sekolah.Noreg,
-                    sekolah.NamaSek,
-                    sekolah.KodeSek,
-                    jurusan.NamaJurusan,
-                    pembayaran_sekolah.Nopembayaran,
-                    detail_bayar_sekolah.NodetailBayar,
-                    mssiswa.NMSISWA
-                    FROM
-                    pembayaran_sekolah
-                    INNER JOIN detail_bayar_sekolah ON pembayaran_sekolah.Nopembayaran = detail_bayar_sekolah.Nopembayaran
-                    INNER JOIN jenispembayaran ON detail_bayar_sekolah.kodejnsbayar = jenispembayaran.Kodejnsbayar
-                    INNER JOIN jurnal ON jenispembayaran.no_jurnal = jurnal.no_jurnal
-                    INNER JOIN sekolah ON pembayaran_sekolah.kodesekolah = sekolah.KodeSek
-                    INNER JOIN jurusan ON sekolah.Jurusan = jurusan.Kodejurusan
-                    INNER JOIN mssiswa ON pembayaran_sekolah.Noreg = mssiswa.Noreg 
-                    WHERE pembayaran_sekolah.Nopembayaran='$bukti'
-                    ORDER BY pembayaran_sekolah.Nopembayaran")->result_array();
-            foreach ($datanil2 as $value) { ?>
-                <td><?= $value['kode_jurnal'] ?></td>
-            <?php } ?>
-
-            <?php
-            $datanya = $this->db->query("SELECT
+                $insert = $this->model_trx_jurnal->insert($mydata, 'akuntansi');
+                if ($insert) {
+                    $datanya = $this->db->query("SELECT
                     jurnal.kode_jurnal,
                     jurnal.nama_jurnal,
                     (SELECT z.NAMA_REV FROM msrev z WHERE z.KETERANGAN=jurnal.JR AND z.`STATUS`=7) AS JR,
@@ -137,19 +107,35 @@ class Trx_jurnal extends CI_Controller
                     INNER JOIN mssiswa ON pembayaran_sekolah.Noreg = mssiswa.Noreg
                     WHERE pembayaran_sekolah.Nopembayaran='$nopem'
                     ORDER BY pembayaran_sekolah.Nopembayaran")->result_array();
-                if($datanya){
-                    $datainsert2 = array(
-                        'no_akuntansi' => '',
-                        'rek' => '',
-                        'urai' => $this->input->post(''),
-                        'dk'   => 'K',
-                        'kurs' => 'ID',
-                        'nilai' => $this->input->post('nilai'),
-                        'tgl_input' => date('Y-m-d'),
-                        'UserId' => $this->session->userdata('nip')
-                    );
-                }
-
+                    if ($datanya) {
+                        $datainsert2 = array(
+                            'no_akuntansi' => $nopem,
+                            'no' => $datanya[0]['NodetailBayar'],
+                            'rek' => $kod,
+                            'urai' => $this->input->post('uraian_2'),
+                            'dk'   => 'K',
+                            'kurs' => 'ID',
+                            'nilai' => $datanya[0]['nominalbayar'],
+                            'tgl_input' => date('Y-m-d'),
+                            'UserId' => $this->session->userdata('nip')
+                        );
+                        $insert = $this->model_trx_jurnal->insert($datainsert2,'detail_akuntansi');
+                        if($insert){
+                            $datainsert3 = array(
+                                'no_akuntansi' => $nopem,
+                                'no' => 0,
+                                'rek' => $kod,
+                                'urai' => $this->input->post('uraian'),
+                                'dk'   => 'D',
+                                'kurs' => 'ID',
+                                'nilai' => $this->input->post('nilai'),
+                                'tgl_input' => date('Y-m-d'),
+                                'UserId' => $this->session->userdata('nip')
+                            );
+                             $insert = $this->model_trx_jurnal->insert($datainsert3,'detail_akuntansi');
+                             echo $a="Data Berhasil di Simpan";
+                        }
+                    }
                 }
             }
         } else {
