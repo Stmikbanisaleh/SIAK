9f68f7e8ba7458596587b117064b1fe68d7f83a8
19332696 <dediprasetio03@gmail.com>
2020-07-01 00:52:32 +0700

    mokup

---
+++ application/views/pagepayroll/slip_gaji/laporan.php
@@ -0,0 +1,672 @@
+<html>
+<head>
+<style>
+	.baris1{
+			clear:both;
+			margin:1px;
+	}
+
+	.baris2{
+			clear:both;
+			margin:1px;
+	}
+
+	.baris3{
+			clear:both;
+			margin:1px;
+	}
+
+   .content_left{
+	height:9.6cm;
+	width:48%;
+	/* border: 2px solid red; */
+	/* margin: 9px; */
+	float: left;
+	margin-right:5px;
+	margin-bottom:5px;
+	background-clip: padding-box;
+   }
+
+   .content_right{
+	height:9.6cm;
+	width:48%;
+	/* border: 2px solid red; */
+	margin-left:5px;
+	margin-bottom:5px;
+	float: right;
+	background-clip: padding-box;
+   }
+
+   .informasi{
+		width:100%;
+   }
+
+   .isidata{
+		width:100%;
+   }
+
+   .footerslip{
+		width:100%;
+		margin-top:20px;
+   }
+
+   td{
+	   font-size:10px;
+   }
+</style>
+</head>
+<body>
+<div class="baris1">
+	<div class="content_left">
+		<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<div class="content_right">
+	<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
+<div class="baris2">
+	<div class="content_left">
+	<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<div class="content_right">
+	<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
+<div class="baris3">
+	<div class="content_left">
+	<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<div class="content_right">
+	<div>
+			<center><font size="3">SLIP GAJI PT. ANDARA KUSUMA ABADI<font></center>
+			<hr></hr>
+		</div>
+		
+		<div class="informasi">
+			<table style="width:100%; float:left;">
+				<tr>
+					<td style="">NIK</td>
+					<td>:</td>
+					<td style="">201110</td>
+					<td style="text-align:right">Januari 2012</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Nama</td>
+					<td>:</td>
+					<td>Dia Abadi Sanjaya Putra Nugraha Danternyata Bukan</td>
+				</tr>
+				<tr style="width:70%">
+					<td>Jabatan</td>
+					<td>:</td>
+					<td>Ketua</td>
+				</tr>
+			</table>
+			<hr style="margin-top:62px;"></hr>
+		</div>
+		<div class="isidata">
+			<table>
+				<tr>
+					<td width="10px;">No</td>
+					<td width="250px;">Keterangan</td>
+					<td>Nilai(Rp)</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>Gaji Pokok</td>
+					<td>50.000.000</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Penambahan Gaji</td>
+					<td>4.000.000</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Potongan</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>4</td>
+					<td>Tunjangan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+				<tr>
+					<td>5</td>
+					<td>Potongan PPh21</td>
+					<td>2.000.000</td>
+				</tr>
+			</table>
+			<hr style="margin-top:10px;"></hr>
+			<table>
+				<tr>
+					<td width="260px;">Total Pendapatan</td>
+					<td>100.000.000</td>
+				</tr>
+			</table>
+		</div>
+		<div class="footerslip">
+			<table>
+				<tr>
+					<td width="160px; text-align:center;">Penerima</td>
+					<td width="160px; text-align:center;">Keuangan</td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="150px; text-align:center;"></td>
+					<td width="150px; text-align:center;"></td>
+				</tr>
+				<tr>
+					<td width="160px; text-align:center;">(.......................................)</td>
+					<td width="160px; text-align:center;">(.......................................)</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
+</body>
+</html>
