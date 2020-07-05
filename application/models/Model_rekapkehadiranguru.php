fe3ba03a81463370b3c351ffeaccefe91158c453
19332696 <dediprasetio03@gmail.com>
2020-07-06 00:50:49 +0700

    rekap keh guru, min header

--- application/models/Model_rekapkehadiranguru.php
+++ application/models/Model_rekapkehadiranguru.php
@@ -18,7 +18,30 @@ class Model_rekapkehadiranguru extends CI_model
     public function view_kehadiranguru($tahun, $blnawal, $blnakhir, $guru){
 
         if($guru == 'none'){
-            return $this->db->query("select b.GuruNama,d.nama,c.hari,a.ID,e.RUANG,f.SINGKTBPS,c.nmklstrjdk as kelas,sum(d.jam) as jml_jam  from trdsrm a 
+            return $this->db->query("select b.GuruNama,d.nama, c.JAM jam, sum(a.WKTHADIR) hadir, c.hari,a.ID,e.RUANG,f.SINGKTBPS,c.nmklstrjdk as kelas,sum(d.jam) as jml_jam, a.TAMBAHAN  from trdsrm a 
+            join tbguru b on a.IdGuru = b.IdGuru 
+            join tbjadwal c on a.idJadwal = c.id
+            join mspelajaran d on c.id_mapel = d.id_mapel 
+            join msruang e on c.id_ruang = e.ID
+            join tbps f on c.ps = f.KDTBPS
+            where YEAR(a.TGLHADIR) = '".$tahun."' and Month(a.TGLHADIR) between '".$blnawal."' and '".$blnakhir."'
+            GROUP BY b.GuruNama,d.nama,c.hari,e.RUANG,f.SINGKTBPS,c.nmklstrjdk, a.idJadwal");
+        } else {
+            return $this->db->query("select b.GuruNama,d.nama, c.JAM jam, sum(a.WKTHADIR) hadir, c.hari,a.ID,e.RUANG,f.SINGKTBPS,c.nmklstrjdk as kelas,sum(d.jam) as jml_jam, a.TAMBAHAN  from trdsrm a 
+            join tbguru b on a.IdGuru = b.IdGuru 
+            join tbjadwal c on a.idJadwal = c.id
+            join mspelajaran d on c.id_mapel = d.id_mapel 
+            join msruang e on c.id_ruang = e.ID
+            join tbps f on c.ps = f.KDTBPS
+            where YEAR(a.TGLHADIR) = '".$tahun."' and Month(a.TGLHADIR) between '".$blnawal."' and '".$blnakhir."' and a.IdGuru = '".$guru."' GROUP BY b.GuruNama,d.nama,c.hari,e.RUANG,f.SINGKTBPS,c.nmklstrjdk, a.idJadwal");
+        }
+    }
+
+    public function view_kehadiranguru_distinct($tahun, $blnawal, $blnakhir, $guru){
+
+        if($guru == 'none'){
+            return $this->db->query("select distinct a.IdGuru, b.GuruNama
+            from trdsrm a 
             join tbguru b on a.IdGuru = b.IdGuru 
             join tbjadwal c on a.idJadwal = c.id
             join mspelajaran d on c.id_mapel = d.id_mapel 
@@ -26,7 +49,8 @@ class Model_rekapkehadiranguru extends CI_model
             join tbps f on c.ps = f.KDTBPS
             where YEAR(a.TGLHADIR) = '".$tahun."' and Month(a.TGLHADIR) between '".$blnawal."' and '".$blnakhir."' ");
         } else {
-            return $this->db->query("select b.GuruNama,d.nama,c.hari,a.ID,e.RUANG,f.SINGKTBPS,c.nmklstrjdk as kelas,sum(d.jam) as jml_jam  from trdsrm a 
+            return $this->db->query("select distinct a.IdGuru, b.GuruNama
+            from trdsrm a 
             join tbguru b on a.IdGuru = b.IdGuru 
             join tbjadwal c on a.idJadwal = c.id
             join mspelajaran d on c.id_mapel = d.id_mapel 
