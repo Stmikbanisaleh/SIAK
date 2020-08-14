05d7165d715503b8a8108bd46b2407ecba7b0f73
imamsatrianta <imamsatrianta@gmail.com>
2020-08-14 10:26:42 +0700

    update

--- application/models/Model_jadwal.php
+++ application/models/Model_jadwal.php
@@ -82,7 +82,7 @@ class Model_jadwal extends CI_model
 
     public function getmapelaktif($data)
     {
-        return  $this->db->query("select a.ID,b.id_mapel , b.nama from trmka a join mspelajaran b on a.KDMKTRMKA = b.id_mapel where PSTRMKA ='".$data."' and b.isdeleted !=1 ");
+        return  $this->db->query("select a.ID,b.id_mapel , b.nama from trmka a join mspelajaran b on a.KDMKTRMKA = b.id_mapel where PSTRMKA ='".$data."' and b.isdeleted !=1  and a.isdeleted != 1");
         
     }
 
