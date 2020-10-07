b72cdb9d6b2f963cda8b9f7969e58d674950d9f9
19332696 <dediprasetio03@gmail.com>
2020-10-07 12:44:29 +0700

    rekap gaji

--- application/controllers/modulpayroll/Rekap_gajikaryawan.php
+++ application/controllers/modulpayroll/Rekap_gajikaryawan.php
@@ -20,7 +20,7 @@ class Rekap_gajikaryawan extends CI_Controller
 		if ($this->session->userdata('username_payroll') != null && $this->session->userdata('nama') != null) {
 			$my_sekolah = $this->model_rekapgkar->view_sekolah()->result_array();
 			$this->load->model('payroll/model_honorguru');
-			$myunit = $this->model_honorguru->view_unit2();
+			$myunit = $this->model_rekapgkar->view_unit()->result_array();
 			$data = array(
 				'page_content' 	=> '../pagepayroll/rekap_gajikaryawan/view',
 				'ribbon' 		=> '<li class="active">Rekap Gaji Karyawan</li><li>Rekap Gaji Karyawan</li>',
--- application/models/payroll/Model_rekapgkar.php
+++ application/models/payroll/Model_rekapgkar.php
@@ -54,24 +54,46 @@ class Model_rekapgkar extends CI_model
         $this->db->truncate($table);
     }
 
+    //OLD
+    // public function view_rekapkaryawan($tahun, $bulan_awal, $bulan_akhir, $unit)
+    // {
+    //     return $this->db->query("select
+    //                                 MONTH(awal_kerja) bulan_awal,
+    //                                 MONTH(akhir_kerja) bulan_akhir,
+    //                                 tk.no_rekening,
+    //                                 tp.*
+    //                             FROM
+    //                                 tb_pendapatan_karyawan tp
+    //                                 JOIN tarifkaryawan tk ON tp.employee_number = tk.id_karyawan
+    //                                 JOIN biodata_karyawan bk ON bk.nip = tp.employee_number
+    //                                 JOIN tbps ps ON bk.unit_kerja = ps.KDSK
+    //                             WHERE
+    //                                 tp.isDeleted != 1
+    //                                 AND ps.KDUNIT = $unit
+    //                                 AND MONTH(effective_date) >= $bulan_awal
+    //                                 AND MONTH(effective_date) <= $bulan_akhir
+    //                                 AND YEAR(effective_date) = $tahun");
+    // }
+    //OLD
+
     public function view_rekapkaryawan($tahun, $bulan_awal, $bulan_akhir, $unit)
     {
         return $this->db->query("select
-                                    MONTH(awal_kerja) bulan_awal,
+                                    tk.id_karyawan, MONTH(awal_kerja) bulan_awal,
                                     MONTH(akhir_kerja) bulan_akhir,
                                     tk.no_rekening,
                                     tp.*
                                 FROM
                                     tb_pendapatan_karyawan tp
                                     JOIN tarifkaryawan tk ON tp.employee_number = tk.id_karyawan
-                                    JOIN biodata_karyawan bk ON bk.nik = tp.employee_number
-                                    JOIN tbps ps ON bk.unit_kerja = ps.KDSK
+                                    JOIN biodata_karyawan bk ON bk.nip = tp.employee_number
+                                    JOIN sekolah ps ON bk.unit_kerja = ps.id
                                 WHERE
                                     tp.isDeleted != 1
-                                    AND ps.KDUNIT = $unit
-                                    AND MONTH(effective_date) >= $bulan_awal
-                                    AND MONTH(effective_date) <= $bulan_akhir
-                                    AND YEAR(effective_date) = $tahun");
+                                    AND ps.id = $unit
+                                    AND MONTH(tp.effective_date) >= $bulan_awal
+                                    AND MONTH(tp.effective_date) <= $bulan_akhir
+                                    AND YEAR(tp.effective_date) = $tahun");
     }
 
     public function view_sekolah()
@@ -82,6 +104,11 @@ class Model_rekapgkar extends CI_model
     public function view_sekolah_one($id)
     {
         return $this->db->query("SELECT DISTINCT s.id, s.deskripsi FROM biodata_karyawan bk, sekolah s WHERE s.id = bk.unit_kerja AND s.id = $id");
+    }
+
+    public function view_unit()
+    {
+        return $this->db->query("SELECT s.id, s.deskripsi FROM sekolah s ORDER BY s.id");
     }
 
     public function view_count($field, $table, $data_id)
