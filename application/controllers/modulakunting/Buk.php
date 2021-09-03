7a8b610722315f5d8a67a2e466429e99976cb45f
imamsatrianta <imamsatrianta@gmail.com>
2021-09-03 20:57:24 +0700

    update

--- application/controllers/modulakunting/Buk.php
+++ application/controllers/modulakunting/Buk.php
@@ -69,8 +69,8 @@ class Buk extends CI_Controller
         $v_bln=date_format($date,"m");
         $f_bln = $v_bln;
         $s_bln = ltrim($f_bln, '0');
-        $query="SELECT * FROM detail_akuntansi where no_akuntansi not in (select no_akuntansi from transaksi_buk) and  tgl_input between '".$datee."' AND '".$datee2." ' ";   
-			$hasil = $this->model_buk->dyn_query($query)->result_array();
+        $query="SELECT * FROM detail_akuntansi where no_akuntansi not in (select no_bukti from transaksi_buk) and  tgl_input between '".$datee."' AND '".$datee2." ' ";   
+        $hasil = $this->model_buk->dyn_query($query)->result_array();
             $no=1;
             foreach ($hasil as $r) {
                 $sql="INSERT INTO transaksi_buk(
@@ -178,8 +178,8 @@ class Buk extends CI_Controller
                 WHERE bukti='".$r['no_akuntansi']."'";
     
                 $this->model_buk->dyn_query($sql3); 
-                $a=1;
             }
+            $a=1;
             echo json_encode($a);
     }
 
